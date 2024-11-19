9c1eaaa30b1a4d54f6cf1a9fcc0af438f6678637
mostafa <mosmosadegh@gmail.com>
2024-11-19 23:22:15 +0330

    handel login in client & Server

---
+++ src/app/api/auth/signin/route.js
@@ -0,0 +1,70 @@
+import connectToDb from "@/configs/db";
+import UserModel from "@/models/User";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+  validateEmail,
+  validatePassword,
+  verifyPassword,
+} from "@/utils/auth";
+
+export async function POST(req) {
+  try {
+    await connectToDb();
+    const body = await req.json();
+    const { email, password } = body;
+
+    //Validation
+    const isValidEmail = validateEmail(email);
+    const isValidPassword = validatePassword(password);
+
+    if (!isValidEmail || !isValidPassword) {
+      return Response.json(
+        { message: "Email or Password is invalid" },
+        { status: 419 }
+      );
+    }
+
+    //1-isUser Exist
+    //3-Generation Token
+    //4-login
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return Response.json({ message: "User not found" }, { status: 422 });
+    }
+
+    const isCorrectPasswordWithHash = await verifyPassword(
+      password,
+      user.password
+    );
+    if (!isCorrectPasswordWithHash) {
+      return Response.json(
+        { message: "userName or password is not correct !!" },
+        { status: 401 }
+      );
+    }
+    const accessToken = generateAccessToken({ email: user.email });
+    const refreshToken = generateRefreshToken({ email: user.email });
+
+    await UserModel.findOneAndUpdate({email},{
+      $set:{
+        refreshToken
+      }
+    })
+
+    return Response.json(
+      { message: "User Logged In successfully" },
+      {
+        status: 200,
+        headers: {
+          "Set-Cookie": `token=${accessToken}; path=/; httpOnly=true`,
+        },
+      }
+    );
+  } catch (error) {
+    console.log("Error", error);
+    return Response.json({ message: "Error Login User" }, { status: 500 });
+  }
+}
--- src/components/templates/login-register/Login.js
+++ src/components/templates/login-register/Login.js
@@ -3,11 +3,78 @@ import React, { useState } from "react";
 import styles from "./login.module.css";
 import Link from "next/link";
 import Sms from "./Sms";
+import { showSwal } from "@/utils/helpers";
+import { validateEmail, validatePassword } from "@/utils/auth";
+
+
 const Login = ({ showRegisterForm }) => {
+  
   const [isLoginWithOtp, setIsLoginWithOtp] = useState(false);
+  const [phoneOrEmail, SetPhoneOrEmail] = useState("");
+  const [password, SetPassword] = useState("");
+
+
   const hideOtpForm = () => {
     setIsLoginWithOtp(false);
   };
+
+  const loginWithPassword = async () => {
+    if(!phoneOrEmail){
+      return showSwal(
+        "لطفا شماره تماس یا ایمیل را وارد کنید",
+        "error",
+        "تلاش مجدد"
+      );
+    }
+    const isValidEmail = validateEmail( phoneOrEmail);
+    if (!isValidEmail) {
+      return showSwal("ایمیل صحیح نمیباشد", "error", "تلاش مجدد");
+    }
+    if(!password){
+      return showSwal(
+        "لطفا پسورد را وارد کنید",
+        "error",
+        "تلاش مجدد"
+      );
+    }
+    const isValidPassword = validatePassword(password);
+    if (!isValidPassword) {
+      return showSwal(" رمز دارای شرایط لازم نمیباشد", "error", "تلاش مجدد");
+    }
+    const user = { email: phoneOrEmail, password };
+
+    const res = await fetch("/api/auth/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    if (res.status === 200) {
+      SetPhoneOrEmail("");
+      SetPassword("");
+      showSwal(
+        "شما با موفقیت وارد حساب خود شدید",
+        "success",
+        "ورود به پنل کاربری"
+      );
+      
+    }else if(res.status === 422 || res.status === 401){
+      showSwal(
+        "کاربری با این اطلاعت یافت نشد",
+        "error",
+        "تلاش مجدد"
+      );
+    }else if(res.status === 419 ){
+      showSwal(
+        "ایمیل یا رمزورود صحیح نمی باشد",
+        "error",
+        "تلاش مجدد"
+      );
+  };
+
+
+  }
 
   return (
     <>
@@ -17,18 +84,22 @@ const Login = ({ showRegisterForm }) => {
             <input
               className={styles.input}
               type="text"
+              value={phoneOrEmail}
+              onChange={(e) => SetPhoneOrEmail(e.target.value)}
               placeholder="ایمیل/شماره موبایل"
             />
             <input
               className={styles.input}
               type="password"
+              value={password}
+              onChange={(e) => SetPassword(e.target.value)}
               placeholder="رمز عبور"
             />
             <div className={styles.checkbox}>
               <input type="checkbox" name="" id="" />
               <p>مرا به یاد داشته باش</p>
             </div>
-            <button className={styles.btn}>ورود</button>
+            <button onClick={loginWithPassword} className={styles.btn}>ورود</button>
             <Link href={"/forget-password"} className={styles.forgot_pass}>
               رمز عبور را فراموش کرده اید؟
             </Link>
